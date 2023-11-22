588342c326c4324eafc87ea391c784e88efe8ef7
Inna Isareva <inna8110302@gmail.com>
2023-11-22 19:12:40 +0200

    added Note model + admin

--- note/models.py
+++ note/models.py
@@ -1,3 +1,18 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Note(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.CharField(max_length=510, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    # user = models.ForeignKey(
+    #     settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    # )
+
+    def __str__(self) -> str:
+        return str(self.title)
+
+    class Meta:
+        ordering = ["-created_at"]

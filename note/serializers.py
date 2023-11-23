c8b38d81be453490b6dd6d1aaf06b176289e1e98
Inna Isareva <inna8110302@gmail.com>
2023-11-23 12:06:19 +0200

    add all field to note serializer

--- note/serializers.py
+++ note/serializers.py
@@ -6,4 +6,10 @@ from note.models import Note
 class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
-        fields = '__all__'
+        fields = (
+            "id",
+            "title",
+            "content",
+            "created_at",
+            "updated_at",
+        )

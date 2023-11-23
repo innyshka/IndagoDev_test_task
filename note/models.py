fcdb470dbe09026078fdfbe5143009aebc1392bc
Inna Isareva <inna8110302@gmail.com>
2023-11-23 12:05:51 +0200

    renamed author to user

--- note/models.py
+++ note/models.py
@@ -7,7 +7,7 @@ class Note(models.Model):
     content = models.CharField(max_length=510, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey(
+    user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE
     )
 

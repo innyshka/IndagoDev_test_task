7271350e89cbc14478d896b4e6556bbb62f011eb
Inna Isareva <inna8110302@gmail.com>
2023-11-23 11:33:01 +0200

    added author to note model

--- note/models.py
+++ note/models.py
@@ -6,13 +6,14 @@ class Note(models.Model):
     title = models.CharField(max_length=255)
     content = models.CharField(max_length=510, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
-    # user = models.ForeignKey(
-    #     settings.AUTH_USER_MODEL, on_delete=models.CASCADE
-    # )
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
 
     def __str__(self) -> str:
-        return str(self.title)
+        return self.title
 
     class Meta:
         ordering = ["-created_at"]
+

6173cb03f149bce4100c940066df524f4342320a
Inna Isareva <inna8110302@gmail.com>
2023-11-23 12:07:11 +0200

    added perform create and pagination

--- note/views.py
+++ note/views.py
@@ -1,16 +1,23 @@
-from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+
 from .models import Note
 from .serializers import NoteSerializer
 
 
-class NoteListCreateView(generics.ListCreateAPIView):
-    queryset = Note.objects.all()
-    serializer_class = NoteSerializer
-    permission_classes = [IsAuthenticated]
+class OrderPagination(PageNumberPagination):
+    page_size = 10
+    max_page_size = 100
 
 
-class NoteDetailView(generics.RetrieveUpdateDestroyAPIView):
+class NoteViewSet(
+    ModelViewSet
+):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
+    pagination_class = OrderPagination
     permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer) -> None:
+        serializer.save(user=self.request.user)
